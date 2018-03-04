cd14d68d8f2b670127c4ae54d5c273688469e34d
stoically <stoically@protonmail.com>
2018-03-04 03:55:29 +0100

    Cache instruments to disk first

--- src/nyc.js
+++ src/nyc.js
@@ -12,44 +12,58 @@ const _mkdirp = require('mkdirp');
 const _execFile = require('child_process').execFile;
 const writeFile = util.promisify(fs.writeFile);
 const mkdirp = util.promisify(_mkdirp);
-const execFile = util.promisify(_execFile);
 const readFile = util.promisify(fs.readFile);
+const execFile = util.promisify(_execFile);
+const crypto = require('crypto');
 
 const instrumentCache = new Map();
 
 class nyc {
   constructor() {
     this._coverage_id = 0;
+    this._nyc_config = false;
+    if (process.env.NYC_CONFIG) {
+      this._nyc_config = JSON.parse(process.env.NYC_CONFIG);
+      this._instrumentsCachePath = path.resolve(path.join(this._nyc_config.tempDirectory, 'instruments'));
+      mkdirp(this._instrumentsCachePath);
+    }
   }
 
   async instrument(sourcePath) {
     if(!instrumentCache.has(sourcePath)) {
-      if(!process.env.NYC_CONFIG) {
+      if(!this._nyc_config) {
         const source = await readFile(sourcePath, 'utf8');
         instrumentCache.set(sourcePath, source);
-      }
-      else {
-        const instrumented = await execFile(process.execPath, [
+      } else {
+        // first pipe to disk and then read it
+        // huge files with a lot of stdout make problems otherwise
+        const sourceMd5 = crypto.createHash('md5').update(sourcePath).digest('hex');
+        const instrumentCachePath = path.join(this._instrumentsCachePath, sourceMd5);
+        await execFile(process.execPath, [
           './node_modules/.bin/nyc',
           'instrument',
-          sourcePath
+          sourcePath,
+          '>',
+          instrumentCachePath
         ], {
           cwd: process.cwd(),
-          env: process.env
+          env: process.env,
+          shell: true
         });
-        instrumentCache.set(sourcePath, instrumented.stdout.toString('utf-8'));
+        const source = await readFile(instrumentCachePath, 'utf8');
+        instrumentCache.set(sourcePath, source);
       }
     }
     return instrumentCache.get(sourcePath);
   }
 
   async writeCoverage(window) {
-    if (!process.env.NYC_CONFIG) {
+    if (!this._nyc_config) {
       return;
     }
-    const nycConfig = JSON.parse(process.env.NYC_CONFIG);
-    await mkdirp(nycConfig.tempDirectory);
-    await writeFile(path.join(nycConfig.tempDirectory, `${Date.now()}_${process.pid}_${++this._coverage_id}.json`), JSON.stringify(window.__coverage__), 'utf-8');
+    const jsonFile = `${Date.now()}_${process.pid}_${++this._coverage_id}.json`;
+    const coverageJson = JSON.stringify(window.__coverage__);
+    await writeFile(path.join(this._nyc_config.tempDirectory, jsonFile), coverageJson, 'utf-8');
   }
 }
 
